3e21d29b06c61f49635c90d4f5ff4f02bee623df
qaqtech <techqaq@gmail.com>
2025-08-22 13:25:51 +0530

    dks changes

--- controller/dks.controller.js
+++ controller/dks.controller.js
@@ -5,14 +5,14 @@ const axios = require('axios');
     let serviceObj = req.body.serviceObj || {};
     var serviceObjKeys=Object.keys(serviceObj) || [];
     let outJson = {};
-    console.log("serviceObjKeys",serviceObjKeys)
+    //console.log("serviceObjKeys",serviceObjKeys)
 
     if(serviceObjKeys.length > 0){
         let methodParam = {};
         methodParam["bodyParams"] = serviceObj["bodyParams"];
         methodParam["service_url"] = serviceObj["service_url"];
         let dksResult = await execGetAxiOsRequestMethod(methodParam);
-        console.log("dksResult",dksResult);
+        //console.log("dksResult",dksResult);
         callback(null,dksResult);
     } else if(serviceObjKeys.length == 0){
         outJson["result"] = '';
@@ -46,9 +46,9 @@ async function getAxiOsRequestMethod(paramJson, callback){
 
         form.append(key, val);
     }
-   console.log("service_url",service_url);
+   //console.log("service_url",service_url);
    //console.log("bodyParams",bodyParams);
-    console.log("Form Data",form) 
+    //console.log("Form Data",form) 
     //const {data} = await Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36
     axios.post(service_url, form, {
         headers: {
@@ -57,13 +57,13 @@ async function getAxiOsRequestMethod(paramJson, callback){
             'secureProtocol': 'TLSv1_3_method'
         }
     }).then(function (response) {
-        console.log(response.data);
+        //console.log(response.data);
         outJson["result"] = response.data;   
         outJson["message"]="SUCCESS";
         outJson["status"]="SUCCESS";
         callback(null,outJson);   
     }).catch(function (error) {
-        console.log(error);
+        //console.log(error);
         outJson["message"]=error.message;
         outJson["status"]="FAIL";
         callback(null,outJson);   
