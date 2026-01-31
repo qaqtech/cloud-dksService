8d5d3a0764ea4c8be8233b28b3a55ee71016688f
DKSLLP <support@dks.ltd>
2026-01-31 10:36:15 -0400

    Req. Debug

--- router/dks.router.js
+++ router/dks.router.js
@@ -4,5 +4,18 @@ var redirect = require('../redirect/dks.redirect');
 
 router.post("/load", redirect.load);
 
+router.post("/echo", (req, res) => {
+  return res.json({
+    ok: true,
+    url: req.originalUrl,
+    method: req.method,
+    headers: {
+      "content-type": req.headers["content-type"],
+      "x-request-id": req.headers["x-request-id"],
+      "user-agent": req.headers["user-agent"],
+    },
+    body: req.body,
+  });
+});
 
 module.exports = router;   
